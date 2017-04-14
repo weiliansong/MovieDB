d5d9ccc6a0f805fe0d32d798f6a4bd9f7ba6368c
Weilian Song <weiliansong@hotmail.com>
2017-04-13 23:54:32 -0400

    Fixed some django stuff

--- src/moviedb/moviedb/urls.py
+++ src/moviedb/moviedb/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^login/', include('login.urls')),
+    url(r'^home/', include('home.urls')),
     url(r'^admin/', admin.site.urls),
 ]
