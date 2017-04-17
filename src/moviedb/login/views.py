40c0915348493fbae339b631f4bf9028ed28f86c
Weilian Song <weilian.song@uky.edu>
2017-04-17 10:49:26 -0400

    Played around with MySQLdb

--- src/moviedb/login/views.py
+++ src/moviedb/login/views.py
@@ -1,15 +1,29 @@
+import MySQLdb
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 def index(request):
-    context = {}
-    return render(request, 'login/index.html', context)
+    return render(request, 'login/index.html')
 
 def log_in(request):
-		print(request.POST['username'])	
-		return HttpResponseRedirect(reverse('home:index'))
+    q_username = request.POST['username']
+    q_password = request.POST['password']
+
+    db = MySQLdb.connect(host='localhost',
+                         user='moviedb_user',
+                         passwd='moviedb_user',
+                         db='moviedatabase')
+    cur = db.cursor()
+    cur.execute('select username from user')
+    queries = cur.fetchall()
+
+    for q in queries:
+      if q_username in q:
+        return HttpResponseRedirect(reverse('home:index'))
+      else:
+        print('Not found')
 
 def register(request):
-		print(request.POST['username'])
-		return HttpResponseRedirect(reverse('home:index'))
+    print(request.POST['username'])
+    return HttpResponseRedirect(reverse('home:index'))
